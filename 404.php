3a02a06da34cf120046f18803d6440487a61c9e3
edgarivanespinoza <edgarivanespinoza@gmail.com>
2022-04-06 22:23:18 +0200

    init

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php get_header( );?>
+
+<main class="container">
+    <div class="pagina404">
+        <h1>404 Pagina web no encontrada</h1>
+        <h2>Haga <a href="<?php echo home_url()?>">click aqui</a> para volver a la pagina principal</h2>
+    </div>
+
+</main>
+
+<?php get_footer( );?>
